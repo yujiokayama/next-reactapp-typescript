1cdcaab685f06c421fe313b9f7a779d78da36b23
yuji-okayama <y-okayama@tribeck.jp>
2020-07-26 19:07:42 +0900

    next.jsにTypeScriptを使えるようにする

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react'
+import Head from 'next/head'
+
+const Home: FC = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>hoge</div>
+  </div>
+)
+
+export default Home
